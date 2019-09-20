c2b7a585bbc4a9c8e672401044288043d3789dc0
Jo Beaver <jobeaveris@hotmail.com>
2019-09-20 20:47:06 +0100

    Crete POST form with Name and Location input fields and Save button

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>CRUD tutorial</title>            
+             <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+            <!-- Popper JS -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        </head>
+        <body>
+            <form action="" method="POST">
+                <label>Name</label>
+                <input type="text" name="name" value="Enter your name">
+                <label>Location</label>
+                <input type="text" name="location" value="Enter your location">
+                <button type="submit" name="save">Save</button>
+            </form>
+        </body>
+    </html>
